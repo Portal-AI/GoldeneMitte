bb6252a1153abab184f8348cce5462721e4e0af1
Uliana Iorina <uliana.iorina@tu-braunschweig.de>
2026-09-10 16:00:37 +0200

    implement readFile

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,4 +1,21 @@
+import java.io.*;
+
 public final class FileManager {
-    public static String readFile(String fileName) {return "";}
-    public static void writeFile(String fileName, String content) {}
+    public static String readFile(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                sb.append(line).append(" ");
+            }
+        }
+        catch (IOException error){
+            throw new RuntimeException("Could not read file: " + new File(fileName).getAbsolutePath(), error);
+        }
+
+        return sb.toString();
+    }
+    public static void writeFile(String fileName, String content) {
+
+    }
 }
