502b9e21db37f92ba3b96a5a2bf134bba8aa5f0e
Uliana Iorina <uliana.iorina@tu-braunschweig.de>
2026-09-10 19:29:44 +0200

    Add test in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        String text = FileManager.readFile("input/raetsel1.txt");
+        String[] parts = text.trim().split("\\s+", 5);
+        String box_string = parts[0] + " " + parts[1] + " " + parts[2];
+        int id = Integer.parseInt(parts[3]);
+        String brick_str = parts[4];
+        Box box = Puzzle.parse_box_string(box_string);
+        Brick bricks = Puzzle.parse_brick_string(id, brick_str);
+        System.out.println(text);
     }
 }
