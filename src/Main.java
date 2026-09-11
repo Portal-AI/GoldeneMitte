0bf17b3880baecc6b93fde00917eeb0a6ae79ffe
Uliana Iorina <uliana.iorina@tu-braunschweig.de>
2026-09-11 15:19:57 +0200

    add test with parse_multiple_bricks_string

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         String text = FileManager.readFile("input/raetsel1.txt");
         String[] parts = text.trim().split("\\s+", 5);
         String box_string = parts[0] + " " + parts[1] + " " + parts[2];
-        int id = Integer.parseInt(parts[3]);
-        String brick_str = parts[4];
+        String bricks_string = parts[4];
         Box box = Puzzle.parse_box_string(box_string);
-        Brick bricks = Puzzle.parse_brick_string(id, brick_str);
-        System.out.println(text);
+        Brick[] bricks = Puzzle.parse_multiple_bricks_string(bricks_string);
+        ArrayList<Brick> bricksList = new ArrayList<>(Arrays.asList(bricks));
+        boolean is_solved = Puzzle.solve_puzzle(box, bricksList);
+        System.out.println(is_solved);
+        System.out.println(box.print_box());
     }
 }
